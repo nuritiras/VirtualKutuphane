2ebea786a75f5f30f0336fd827c83967d6dc6b48
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2022-12-15 23:22:04 +0300

    Proje dosyası ekle.

---
+++ VirtualKutuphane/Kutuphane.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKutuphane
+{
+    public class Kutuphane
+    {
+
+        public virtual void Oku()
+        {
+            Console.WriteLine("Okuma işlemini başarıyla gerçekleştirdiniz.");
+        }
+    }
+
+    public class Kitap:Kutuphane
+    {
+        public override void Oku()
+        {
+            Console.WriteLine("Kitap okuma işlemini başarıyla gerçekleştirdiniz.");
+        }
+    }
+
+    public class Dergi:Kutuphane
+    {
+        public override void Oku()
+        {
+            Console.WriteLine("Dergi okuma işlemini başarıyla gerçekleştirdiniz.");
+        }
+    }
+
+    public class Ansiklopedi:Kutuphane
+    {
+        public override void Oku()
+        {
+            Console.WriteLine("Ansiklopedi okuma işlemini başarıyla gerçekleştirdiniz.");
+        }
+    }
+
+
+}
---
+++ VirtualKutuphane/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKutuphane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap= new Kitap();
+            kitap.Oku();
+
+            Dergi dergi = new Dergi();
+            dergi.Oku();
+
+            Ansiklopedi ansiklopedi =   new Ansiklopedi();
+            ansiklopedi.Oku();
+        }
+    }
+}
